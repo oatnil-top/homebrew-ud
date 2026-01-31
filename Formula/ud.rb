c46f2261748c75d3005d52dfa6eee7d496b6855c
Lintao <lintao.amons@outlook.com>
2026-01-31 11:40:50 +0800

    Update ud to 0.21.3

--- Formula/ud.rb
+++ Formula/ud.rb
@@ -4,43 +4,43 @@
 class Ud < Formula
   desc "UnderControl CLI - task and expense management from the terminal"
   homepage "https://github.com/oatnil-top/ud-cli"
-  version "0.21.2"
+  version "0.21.3"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://pub-35d77f83ee8a41798bb4b2e1831ac70a.r2.dev/cli/releases/0.21.2/ud_0.21.2_darwin_arm64.tar.gz"
-      sha256 "f11aa5fc1293aa5334968131a90c8e4f38bf2a86e0061f4dee8a60c5211c04a3"
+      url "https://pub-35d77f83ee8a41798bb4b2e1831ac70a.r2.dev/cli/releases/0.21.3/ud_0.21.3_darwin_arm64.tar.gz"
+      sha256 "2efd3a3e4f022077e52fc928222ff149c6650fbf6ffcab4e72f1d7406cef9f27"
 
       def install
-        bin.install "ud_0.21.2_darwin_arm64" => "ud"
+        bin.install "ud_0.21.3_darwin_arm64" => "ud"
       end
     end
     on_intel do
-      url "https://pub-35d77f83ee8a41798bb4b2e1831ac70a.r2.dev/cli/releases/0.21.2/ud_0.21.2_darwin_amd64.tar.gz"
-      sha256 "7657126561dfd6cbd661e27566c2fa09c8e28eb516c27ad69e5c523d678a3d50"
+      url "https://pub-35d77f83ee8a41798bb4b2e1831ac70a.r2.dev/cli/releases/0.21.3/ud_0.21.3_darwin_amd64.tar.gz"
+      sha256 "b9a0cbd8f40c56b4609ed4f3ee0964a74287ad84abbf35f304be7b64bb180388"
 
       def install
-        bin.install "ud_0.21.2_darwin_amd64" => "ud"
+        bin.install "ud_0.21.3_darwin_amd64" => "ud"
       end
     end
   end
 
   on_linux do
     on_arm do
-      url "https://pub-35d77f83ee8a41798bb4b2e1831ac70a.r2.dev/cli/releases/0.21.2/ud_0.21.2_linux_arm64.tar.gz"
-      sha256 "60938c668d8396fe66383e42fd37d080be42195031b886e6fa48826e59ef2270"
+      url "https://pub-35d77f83ee8a41798bb4b2e1831ac70a.r2.dev/cli/releases/0.21.3/ud_0.21.3_linux_arm64.tar.gz"
+      sha256 "5ffec5962e6539fd1fcdfab0e922a26ea90e9b2aa13e7f34dd372cb8e2a644e6"
 
       def install
-        bin.install "ud_0.21.2_linux_arm64" => "ud"
+        bin.install "ud_0.21.3_linux_arm64" => "ud"
       end
     end
     on_intel do
-      url "https://pub-35d77f83ee8a41798bb4b2e1831ac70a.r2.dev/cli/releases/0.21.2/ud_0.21.2_linux_amd64.tar.gz"
-      sha256 "a780f8c4afc93558a6ea95216b674264ba959a9b5d3c2bc6e110e1fedecac82f"
+      url "https://pub-35d77f83ee8a41798bb4b2e1831ac70a.r2.dev/cli/releases/0.21.3/ud_0.21.3_linux_amd64.tar.gz"
+      sha256 "be898dd97f18b001ca2042a355a7773e728af3679df2a1ec334dc80c0145dd32"
 
       def install
-        bin.install "ud_0.21.2_linux_amd64" => "ud"
+        bin.install "ud_0.21.3_linux_amd64" => "ud"
       end
     end
   end
